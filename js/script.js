8cfd801b6f2960d2d57a963894606373cec6e65d
ChristineWasike <christinewasike22@gmail.com>
2017-06-16 12:07:15 +0300

    commited on the javascript area

---
+++ js/script.js
@@ -0,0 +1,53 @@
+// BUSINESS LOGIC
+// create a player 1 object
+
+
+
+// create a player 2 object
+
+
+
+
+// pick a random number btn 1 and 6 for the roll of the die
+
+
+// create a var that collects the random number
+
+
+// set the score after a single roll
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// USER INETERFACE LOGIC
+
+// if statement if the number is 1
+
+// return the score 0
+
+
+// skip to next player
+
+
+// else statement if the die reads 2-6
+
+// return score as 2-6
+
+// get another turn
+
+// check for winner when either players' score is 100
+
+
+
+// declare the winner
